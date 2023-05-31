5d8a63d171cada7c9d140f3b1e487d1efe6966ea
artemiy <karelskiy@mail.ru>
2023-05-31 22:00:16 +0300

    change zad20

--- main.py
+++ main.py
@@ -67,18 +67,34 @@
 # *Пример:*
 # ноутбук
 #     12
-word = input().upper()
-dictionary = {'A, E, I, O, U, L, N, S, T, R, А, В, Е, И, Н, О, Р, С, Т': 1,
-            'D, G, Д, К, Л, М, П, У': 2,
-            'B, C, M, P, Б, Г, Ё, Ь, Я': 3,
-            'F, H, V, W, Y, Й, Ы': 4,
-            'K, Ж, З, Х, Ц, Ч': 5, 
-            'J, X, Ш, Э, Ю': 8,
-            'Q, Z, Ф, Щ, Ъ': 10}
-result = 0
-for i in word:
-    for key in dictionary:
-        if key in dictionary:
-            if i in key:
-                result += dictionary[key]
-print(f'Ваш результат {result} очка(ов)')
+# word = input().upper()
+# dictionary = {'A, E, I, O, U, L, N, S, T, R, А, В, Е, И, Н, О, Р, С, Т': 1,
+#             'D, G, Д, К, Л, М, П, У': 2,
+#             'B, C, M, P, Б, Г, Ё, Ь, Я': 3,
+#             'F, H, V, W, Y, Й, Ы': 4,
+#             'K, Ж, З, Х, Ц, Ч': 5, 
+#             'J, X, Ш, Э, Ю': 8,
+#             'Q, Z, Ф, Щ, Ъ': 10}
+# result = 0
+# for i in word:
+#     for key in dictionary:
+#         if key in dictionary:
+#             if i in key:
+#                 result += dictionary[key]
+# print(f'Ваш результат {result} очка(ов)')
+
+# второе решение
+dictionary = {1:"AEIOULNSTRABEMHOPCT", 
+              2: "СХЭДКЛМПУ", 
+              3:"ВСМРБГЁЬЯ", 
+              4:"FHVWYAW", 
+              5:"КЖЗХЦЧ", 
+              8:"ЗХШЭЮ", 
+              10:"QZOMHb"}
+word = input("Введите слово: ").upper() 
+sum = 0
+for i in word :
+  for key, value in dictionary.items(): 
+    if i in value:
+      sum += key
+print(f"Стоимость слова: {sum}")
